54646b48c0f44bbcb1ddf5e325a820cf71d49aee
breno5896 <brenosouza47733@gmail.com>
2024-01-03 10:26:53 -0300

    mudança

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function my(){
     const entre=Math.ceil( document.querySelector(".min").value);
     const depois =Math.floor(document.querySelector(".max").value);
+    if(entre>=depois){alert("o valor minimo precisa ser maior do que o do maximo")}
+    else{
 const result= Math.floor(Math.random() * (depois - entre+ 1)) + entre;
 alert(result)
+}
 }
